4a4ace9a7faa6a587061a5399c646324ea70526a
rahulb <aerolith07@gmail.com>
2021-06-03 10:55:20 +0100

    remove storybook

--- src/components/SurveyComponents/atoms/IconButton.tsx
+++
@@ -1,6 +0,0 @@
-
-const IconButton = () => {
-  return
-  <IconButton size="xs" aria-label="Add Radio Option" isDisabled={!canAdd} variant="outline" mt="2" mr="2" colorScheme="green" icon={<AddIcon />} onClick={addOption} />
-
-}
